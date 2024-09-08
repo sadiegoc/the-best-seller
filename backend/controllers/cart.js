d41fd0904b6a8c0f5750d6e68b44716772a1cc6e
Diego Sá <diego.sa1687@hotmail.com>
2024-09-08 20:36:48 -0300

    cart and categories controller

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -2,20 +2,119 @@ const redis = require('redis')
 const redisClient = redis.createClient()
 
 module.exports = app => {
-    const get = async (req, res) => {
+    const { existsOrError } = app.controllers.validation
 
+    const get = async (req, res) => {
+        if (!req.params.uid) res.status(400).send('User not given')
+    
+        const { uid } = req.params
+
+        try {
+            await redisClient.connect()
+
+            const cartData = await redisClient.get(`cart:uid:${uid}`)
+
+            if (cartData) { res.status(200).json(JSON.parse(cartData)) }
+            else { res.status(400).send('Cart is empty.') }
+        } catch (err) {
+            res.status(500).send(err)
+        } finally {
+            await redisClient.quit()
+        }
     }
 
     const save = async (req, res) => {
+        if (!req.params.uid) res.status(400).send('User not given')
         
+        const { uid } = req.params
+        const product = { ...req.body }
+
+        try {
+            existsOrError(product.id, 'Product not given.')
+            existsOrError(product.name, 'Name not given.')
+            existsOrError(product.price, 'Price not given.')
+            existsOrError(product.image_url, 'Image not given.')
+            existsOrError(product.amount, 'Amount not given.')
+
+            await redisClient.connect()
+
+            const cartData = await redisClient.get(`cart:uid:${uid}`)
+
+            let cart = cartData ? JSON.parse(cartData) : []
+            cart.push(product)
+
+            await redisClient.set(`cart:uid:${uid}`, JSON.stringify(cart))
+
+            res.status(200).json(cart)
+        } catch (err) {
+            res.status(500).send(err)
+        } finally {
+            await redisClient.quit()
+        }
     }
 
     const edit = async (req, res) => {
+        if (!req.params.uid) res.status(400).send('User not given')
+        if (!req.params.pid) res.status(400).send('Product not given')
         
+        const { uid, pid } = req.params
+        const amount = req.body.amount
+        
+        try {
+            await redisClient.connect()
+
+            const cartData = await redisClient.get(`cart:uid:${uid}`)
+            if (cartData) {
+                let cart = JSON.parse(cartData)
+                cart = cart.map(product => {
+                    if (product.id === parseInt(pid)) {
+                        return {
+                            ...product,
+                            amount
+                        }
+                    }
+
+                    return product
+                })
+
+                redisClient.set(`cart:uid:${uid}`, JSON.stringify(cart))
+
+                res.status(200).json(cart)
+            } else {
+                res.status(400).send('Cart is empty.')
+            }
+        } catch (err) {
+            res.status(500).send(err)
+        } finally {
+            await redisClient.quit()
+        }
     }
 
     const remove = async (req, res) => {
-        
+        if (!req.params.uid) res.status(400).send('User not given')
+        if (!req.params.pid) res.status(400).send('Product not given')
+
+        const { uid, pid } = req.params
+
+        try {
+            await redisClient.connect()
+
+            const cartData = await redisClient.get(`cart:uid:${uid}`)
+            if (cartData) {
+                let cart = JSON.parse(cartData)
+                cart = cart.filter(product => product.id !== parseInt(pid))
+
+                redisClient.set(`cart:uid:${uid}`, JSON.stringify(cart))
+
+                res.status(200).json(cart)
+            } else {
+                res.status(400).send('Cart not found')
+            }
+        } catch (err) {
+            res.status(500).send(err)
+        } finally {
+            await redisClient.quit()
+        }
     }
 
     return { get, save, edit, remove }
--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,22 +1,60 @@
 module.exports = app => {
-    const get = async (req, res) => {
+    const { existsOrError } = app.controllers.validation
 
+    const get = async (req, res) => {
+        app.db('categories')
+            .select()
+            .then(categories => res.status(200).json(categories))
+            .catch(err => res.status(500).send(err))
     }
 
     const getById = async (req, res) => {
-        
+        if (!req.params.cid) res.status(400).send('Category not given.')
+        const id = req.params.cid
+
+        app.db('categories')
+            .select()
+            .where({ id }).first()
+            .then(category => res.status(200).json(category))
+            .catch(err => res.status(500).send(err))
     }
 
     const save = async (req, res) => {
-        
+        if (!req.body.name) res.status(400).send('Name cannot be empty!')
+        const name = { name: req.body.name }
+
+        app.db('categories')
+            .insert(name)
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).send())
     }
 
     const edit = async (req, res) => {
+        if (!req.params.cid) res.status(400).send('Category not given.')
+        if (!req.body.name) res.status(400).send('Name cannot be empty!')
         
+        const id = req.params.cid
+        const name = { name: req.body.name }
+
+        app.db('categories')
+            .update(name)
+            .where({ id })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(500).send(err))
     }
 
     const remove = async (req, res) => {
-        
+        if (!req.params.cid) res.status(400).send('Category not given.')
+        const id = req.params.cid
+
+        try {
+            const rowsDeleted = await app.db('categories').where({ id }).del()
+            existsOrError(rowsDeleted, 'Category not found.')
+
+            res.status(204).send()
+        } catch (err) {
+            return res.status(500).send(err)
+        }
     }
 
     return { get, getById, save, edit, remove }
